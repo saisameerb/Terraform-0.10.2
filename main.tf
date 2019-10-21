16c96536f816849253425a80d44579e579176619
SaiSameerB <51922935+saisameerb@users.noreply.github.com>
2019-10-21 10:53:04 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,6 +29,8 @@ module "#Component 3" {
   vms               = "${var.*_vms}"
 }
 
+#Sample Template 
+    
 module "oracle_ams" {
   source = "git::http://stash.dev-charter.net/stash/scm/aut/terraform_modules.git?ref=v0.2.0//vmware_adddisk_vm_v2"
   default           = "${merge(var.default, var.oracle_ams_overrides)}"
